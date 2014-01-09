20614340d9b02aa8c7ef3e087549644052a5ece0
jason_yao <jason_yao@htc.com>
2014-01-10 04:19:28 +0800

    update

---
+++ grs/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+long_description = open('./README.rst', 'r').read()
+description = '台灣上市股票價格擷取（Fetch TWSE stock data）' + \
+              '含即時盤、台灣時間轉換、開休市判斷。'
+
+setup(name='grs',
+      version='0.4.1',
+      description=description,
+      long_description=long_description,
+      author='Toomore Chiang',
+      author_email='toomore0929@gmail.com',
+      url='https://github.com/toomore/grs',
+      packages=['grs'],
+      include_package_data=True,
+      license='MIT',
+      keywords="stock taiwan taipei twse 台灣 股市 台北 即時",
+      install_requires=['python-dateutil==1.5'],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Console',
+          'Environment :: Web Environment',
+          'Intended Audience :: End Users/Desktop',
+          'Intended Audience :: Financial and Insurance Industry',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Natural Language :: Chinese (Traditional)',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Office/Business :: Financial :: Investment',
+          ],
+     )
