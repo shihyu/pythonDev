ee4400eb126360eb7bea7a29baf7fcf232850bb1
jasonblog <pythontcl@gmail.com>
2014-12-02 23:16:05 +0800

    update

--- testify/mytest.py
+++ testify/mytest.py
@@ -1,27 +1,25 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
- 
+
 import testify
-from ctypes import *
-from test import * #加载test.py
- 
-class Myclass(testify.TestCase): 
-    global area
-    mylib = CDLL( "./libtest.so" ) 
-    area = mylib.area 
-    def setUp( self ): 
+from ctypes import*
+from test import*  # 加载test.py
+
+class Myclass(testify.TestCase):
+    def setUp(self):
+        self.mylib = CDLL("./libtest.so")
+        self.area = self.mylib.area
         print 'setUp'
-        pass 
- 
-    def tearDown( self ): 
+        pass
+
+    def tearDown(self):
         print 'tearDown'
-        pass 
- 
-    def test1( self ): 
+        pass
+
+    def test1(self):
         print 'test1'
-        rc = rect( point(0, 0), point(1, 1) ) 
-        self.assert_( area( rc ) == 1 ) 
- 
-if __name__ == "__main__": 
-    #unittest.main()
+        rc = rect(point(0, 0), point(1, 1))
+        self.assert_(self.area(rc) == 1)
+
+if __name__ == "__main__":
     run()
